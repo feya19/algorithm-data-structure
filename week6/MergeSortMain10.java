f0f70d508553692ff105ef4738dd96635eca3752
fahmi <fahmi.yahyafl@gmail.com>
2024-04-01 14:53:58 +0700

    WEEK-6: Searching

---
+++ week6/MergeSortMain10.java
@@ -0,0 +1,14 @@
+package week6;
+
+public class MergeSortMain10 {
+    public static void main(String[] args) {
+        int data[] = {10, 40, 30, 50, 70, 20, 100, 90};
+        System.out.println("sorting dengan merge sort");
+        MergeSorting10 mSort = new MergeSorting10();
+        System.out.println("data awal");
+        mSort.printArray(data);
+        mSort.mergeSort(data);
+        System.out.println("setelah diurutkan");
+        mSort.printArray(data);
+    }
+}
